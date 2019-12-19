a0af03bf308a44dd3ed686ff0767ddfc3371785a
Joe Cooper <joe@dhis2.org>
2019-12-19 14:26:42 +0100

    lesson: adding onClick handlers to components
    important takeaways from this lesson:
    - all javascript inside jsx needs to be wrapped within expressions
    - bind functions to stop them being called when the component is rendered
    - the entire file is javascript. functions do not need to be rendered in the html, they can be declared alongside the App() function, for example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Button = props => (
+  //passing an unnamed function, which contains console log, to the onclick. then the console function is not evaluated on render. and don't forget that all js must be in an expression
+  <button onClick={function () {
+    console.log('bite me')
+  }} type="button">{props.children}</button>
+)
+
+const Button2 = props => (
+  //binding an unnamed function to console log. using syntax sugar, but this is the same as the above Button.
+  <button onClick={() => console.log('i like pie')} type="button">{props.children}</button>
+)
+
+const Button3 = props => (
+  //binding a function that is stated elsewhere
+  <button onClick={()=>foo(props.logMe)} type="button">{props.children}</button>
+)
+
+//this whole file is js. so define the functions here, not in the rendered html. lol.
+function foo(a){console.log(a)}
+
+console.log('ready to roll')
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click me</Button>
+      <Button2>222</Button2>
+      <Button3 logMe='doggy doggy'>external function</Button3>
+      <Button3 logMe="i rule the dog land"> ok</Button3>
     </div>
   );
 }
